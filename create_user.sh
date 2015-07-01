6ea6acfed0cebf6b7b95073673fe4287b53bae41
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-01 15:16:47 +0200

    Added user to the sudo group

--- create_user.sh
+++ create_user.sh
@@ -10,6 +10,10 @@ addgroup --gid 1001 genomics
 PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 ENCRYPTED=$(echo "$PASS" | mkpasswd -m sha-512 --stdin)
 useradd  -m -s /bin/bash -c "Frank Foerster" -g genomics -u 1060 -p "$ENCRYPTED" frf53jh
+
+# add user to sudo group
+sudo adduser frf53jh sudo
+
 # Finally set the password to expired
 passwd frf53jh --expire
 # And print the account name and the password
