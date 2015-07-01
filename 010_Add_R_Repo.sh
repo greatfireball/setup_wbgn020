5a65eead45b267d4f7fd138b1a63bc9ad9972c44
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-01 17:23:31 +0200

    Added apt-get update statement

--- 010_Add_R_Repo.sh
+++ 010_Add_R_Repo.sh
@@ -7,3 +7,5 @@ echo "deb http://ftp5.gwdg.de/pub/misc/cran/bin/linux/ubuntu precise/" > /etc/ap
 gpg --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 
+apt-get update
+
