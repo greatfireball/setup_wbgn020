b44e305fdc19c5759dca5fd5dc1e8fc2316304ed
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-01 17:25:39 +0200

    Changed the repo Ubuntu release to trusty

--- 010_Add_R_Repo.sh
+++ 010_Add_R_Repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Add GNU R Mirror
-echo "deb http://ftp5.gwdg.de/pub/misc/cran/bin/linux/ubuntu precise/" > /etc/apt/sources.list.d/gnuR.list
+echo "deb http://ftp5.gwdg.de/pub/misc/cran/bin/linux/ubuntu trusty/" > /etc/apt/sources.list.d/gnuR.list
 
 ## Add GNU R Signing Key
 gpg --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
