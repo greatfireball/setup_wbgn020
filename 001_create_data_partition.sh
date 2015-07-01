024dabffc87c64dad578da285b48abad40d4817a
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-01 16:07:03 +0200

    sdb will now be partitioned

--- 001_create_data_partition.sh
+++ 001_create_data_partition.sh
@@ -2,3 +2,27 @@
 
 # install parted
 apt-get update && apt-get install parted
+
+# create a GTP table
+parted --script --align=opt /dev/sdb mklabel gpt
+# Add a partition starting at 8 MB and fill the whole hd
+parted --script --align=opt /dev/sdb mkpart primary 8MB 100%
+
+# create a file system
+mkfs.ext4 /dev/sdb1
+
+# create the mount point
+mkdir -p /storage
+
+# mount the drive
+mount /dev/sdb1  /storage/
+
+# estimate the UUID for our drive
+eval $(sudo blkid | grep sdb1 | cut -f 2 -d ":")
+
+## This should result in setting of UUID and TYPE as Env varibles
+
+# and add the drive to the fstab to mount it after reboot
+
+date +"#%Y-%m-%dT%H:%M:%S Added by install script to enable remounting on reboot
+UUID=$UUID /storage $TYPE defaults,errors=remount-ro 0 1" >> /etc/fstab
