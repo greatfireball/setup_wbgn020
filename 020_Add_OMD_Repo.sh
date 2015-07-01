b8440b2d1948c99fb6f6143a4dd6f6bab8f9b126
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-01 17:23:51 +0200

    Added apt-get update statement

--- 020_Add_OMD_Repo.sh
+++ 020_Add_OMD_Repo.sh
@@ -2,8 +2,9 @@
 
 ## Add OMD Mirror Repository
 echo 'deb http://labs.consol.de/repo/stable/ubuntu trusty main' > /etc/apt/sources.list.d/omd.list
-apt-get update
 
 ## Add OMD Mirror Key
 gpg --keyserver keyserver.ubuntu.com --recv-keys F8C1CA08A57B9ED7
 gpg --armor --export F8C1CA08A57B9ED7 | apt-key add -
+
+apt-get update
