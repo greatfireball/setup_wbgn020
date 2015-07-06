3d0a3a8d4e91a113cda12e83de04cb6701191c6f
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-06 14:59:52 +0200

    Need to ignore partitions

--- 004_create_zpool.sh
+++ 004_create_zpool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get a list of ids for the raid0 harddrives
-DRIVES=$(ls -l /dev/disk/by-id/ | grep sd[bcdef] | grep scsi | awk '{print $9}' | sed 's/^/\/dev\/disk\/by-id\//g' | tr "\n" " ")
+DRIVES=$(ls -l /dev/disk/by-id/ | grep sd[bcdef] | grep scsi | grep -v -- '-part' | awk '{print $9}' | sed 's/^/\/dev\/disk\/by-id\//g' | tr "\n" " ")
 
 # create a zpool
 zpool create -df -o ashift=12 -O compression=on storage raidz2 $DRIVES
