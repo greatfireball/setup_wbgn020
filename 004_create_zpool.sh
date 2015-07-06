0bcb0cdbbd155e127c528a6d7f58ffcf004519bb
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-06 14:55:36 +0200

    Added creation of a zpool and an additional zfs

---
+++ 004_create_zpool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get a list of ids for the raid0 harddrives
+DRIVES=$(ls -l /dev/disk/by-id/ | grep sd[bcdef] | grep scsi | awk '{print $9}' | sed 's/^/\/dev\/disk\/by-id\//g' | tr "\n" " ")
+
+# create a zpool
+zpool create -df -o ashift=12 -O compression=on storage raidz2 $DRIVES
+
+
