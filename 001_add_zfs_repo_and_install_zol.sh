485112d3b924ae52315885b7fa2a383375def7be
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-06 13:24:57 +0200

    Added a info comment with my reference

--- 001_add_zfs_repo_and_install_zol.sh
+++ 001_add_zfs_repo_and_install_zol.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This installation is based on the instruction on: https://wiki.ubuntuusers.de/ZFS_on_Linux?redirect=no
 apt-add-repository --yes ppa:zfs-native/stable
 apt-get update
 apt-get install --yes ubuntu-zfs
