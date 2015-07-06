22459f8177a3530b9940119f3a01c5bb09f5eb92
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-06 13:25:36 +0200

    Installation of required package added

--- 001_add_zfs_repo_and_install_zol.sh
+++ 001_add_zfs_repo_and_install_zol.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # This installation is based on the instruction on: https://wiki.ubuntuusers.de/ZFS_on_Linux?redirect=no
+
+# install python-software-properties, which contains the apt-add-repository command
+apt-get install --yes python-software-properties
 apt-add-repository --yes ppa:zfs-native/stable
 apt-get update
 apt-get install --yes ubuntu-zfs
