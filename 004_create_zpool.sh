91af27b3ce48ff7b11d35c4a6eaba544f59d21d6
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-06 15:52:53 +0200

    Set compression to lz4 for storage/neurobio and upgraded storage zpool

--- 004_create_zpool.sh
+++ 004_create_zpool.sh
@@ -6,6 +6,11 @@ DRIVES=$(ls -l /dev/disk/by-id/ | grep sd[bcdef] | grep scsi | grep -v -- '-part
 # create a zpool
 zpool create -df -o ashift=12 -O compression=on storage raidz2 $DRIVES
 
+# upgrade the pool (can be achieved by deleting -d in command above)
+zpool upgrade storage
+
 # create an additional zfs device
 zfs create storage/neurobio
 
+# set lz4 as compression method for storage/neurobio
+zfs set compression=lz4 storage/neurobio
