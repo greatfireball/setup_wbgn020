8009c89c080ba7ba2cc1774cf00154252f3a1287
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-07-06 13:25:49 +0200

    Added comments

--- 001_add_zfs_repo_and_install_zol.sh
+++ 001_add_zfs_repo_and_install_zol.sh
@@ -4,6 +4,12 @@
 
 # install python-software-properties, which contains the apt-add-repository command
 apt-get install --yes python-software-properties
+
+# add the ZoL repository
 apt-add-repository --yes ppa:zfs-native/stable
+
+# update package list
 apt-get update
+
+# install ZoL
 apt-get install --yes ubuntu-zfs
